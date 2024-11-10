09126965cf7556c82f6016aea3282937c2f28cfb
danderi <anderi.daniel@gmail.com>
2024-11-10 20:48:25 +0000

    just finished practicing the beginners tutorial

--- exercises/19-Bottles-Of-Milk/app.py
+++ exercises/19-Bottles-Of-Milk/app.py
@@ -1,1 +1,15 @@
 # ✅↓ Write your code here ↓✅
+def number_of_bottles():
+    num = 99
+    while num >= 0:
+        if num == 2:
+            print(f"{num} bottles of milk on the wall, {num} bottles of milk. Take one down and pass it around, {num-1} bottle of milk on the wall.")
+        elif num == 1:
+            print("1 bottle of milk on the wall, 1 bottle of milk. Take one down and pass it around, no more bottles of milk on the wall.")
+        elif num == 0:
+            print("No more bottles of milk on the wall, no more bottles of milk. Go to the store and buy some more, 99 bottles of milk on the wall.")
+        else:
+            print(f"{num} bottles of milk on the wall, {num} bottles of milk. Take one down and pass it around, {num-1} bottles of milk on the wall.")
+        num-=1
+
+number_of_bottles()
